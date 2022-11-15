c48f99444207689c1bad8c79ddebc6f1c8de5c45
mourad_br <mouradbarkouch@gmail.com>
2022-11-15 23:38:51 +0100

    create vars (var, let ,const) and diffrence between them

---
+++ variables.js
@@ -0,0 +1,21 @@
+/*
+var : 
+ - function scope.
+ - can be redeclare.
+ - undifiened when accessing a variables before it's declared.
+ - create properties in window object.
+
+ ________________________
+let : 
+ - block scope.
+ - can't be redeclared.
+ - reference error when accessing in the window object.
+
+ ________________________
+const : 
+ - block scope.
+ - can't be redeclared.
+ - refference error when accessing a variable before it's declare.
+ - does't create properties in the window object.
+
+*/
